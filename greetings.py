faff84096b84f5d8b08b70d613bfa7dccc9dd80d
Xobs152 <vladnice2005@gmail.com>
2024-12-15 23:47:42 +0700

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = "Иван Иванов"
+title = "Покупка продуктов"
+content = "Купить молок, хлеб и яйца"
+status = "Активна"
+created_date = "27-05-2024"
+issue_date = "30-05-2024"
+
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
